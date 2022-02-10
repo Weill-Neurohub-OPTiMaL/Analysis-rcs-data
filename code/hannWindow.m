e6939784e2faf09a228e629ac59bcbd80e0d4c64
tcdixon4 <36492457+tcdixon4@users.noreply.github.com>
2022-02-09 22:22:36 -0800

    fix small bug in creation of Hann window that maintains window size for <100% windows

--- code/hannWindow.m
+++ code/hannWindow.m
@@ -21,21 +21,16 @@ switch percentage
     case '25% Hann'
         temp_win = 0.5*(1-cos(8*pi*(0:L-1)/(L-1))); % create hann taper function, equivalent to the Hann 250% 
         hann_win = sethannwindow(temp_win);  
-end
-
-% adding this to match length of hann window and fft size (needs debugging)
-if length(hann_win) > L && mod(length(hann_win),L) == 2
-    hann_win = hann_win(2:end-1);    
+    case '0% Hann'
+        hann_win = ones([1,L]);
 end
 
 end
 
 function hann_out = sethannwindow(temp_win)
     % sets hann window flat top in case is not at 100% hann window 
-     [maxvals,indeces] = findpeaks(temp_win);
-     temp_win_middle = ones(1,length(indeces(1)-1:indeces(end)-1));
-     temp_win_left  = temp_win(1:indeces(1));
-     temp_win_right = temp_win(indeces(end):end);
-     hann_out = [temp_win_left temp_win_middle temp_win_right];        
+    [~,indeces] = findpeaks(temp_win);
+    temp_win(indeces(1)+1:indeces(end)-1) = 1;
+    hann_out = temp_win;        
 end
         
