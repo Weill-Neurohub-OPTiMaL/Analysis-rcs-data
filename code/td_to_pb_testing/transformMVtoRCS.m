bba18d3d5148896269b4830e2883bf07a1b5d1cb
tcdixon4 <tcdixon44@gmail.com>
2022-02-10 17:19:46 -0800

    Documentation update: function header

--- code/td_to_pb_testing/transformMVtoRCS.m
+++ code/td_to_pb_testing/transformMVtoRCS.m
@@ -1,4 +1,4 @@
-%% Function Name: shifted_ints = transformMVtoRCS(data_mv, amp_gain)
+%% Function Name: data_rcs = transformMVtoRCS(data_mv, amp_gain)
 %
 % Description: Converts data in units of mV to the internal unit 
 % representation used on the RC+S device.
@@ -6,16 +6,21 @@
 % Inputs:
 %     data_mV : (num_samples, num_channels) array, or transpose
 %         Data, either time-domain or FFT amplitude, given in units of mV.
-%         If a two dimensional array is given, the result will be returned
-%         in the same shape.
+%         The result will be returned in the same shape.
 %     amp_gain : int, 
 %         Parameter indicating the channel gain represented by the
 %         cfg_config_data.dev.HT_sns_ampX_gain250_trim value in the
 %         DeviceSettings.json file, or the metaData.ampGains OpenMind
 %         output.
 %
-% Author: Tanner Chas Dixon, tanner.dixon@ucsf.edu
-% Date: February 3, 2022
+% Outputs:
+%     data_rcs : (num_samples, num_channels) array, or transpose
+%         Data, either time-domain or FFT amplitude, given in internal RC+S
+%         units. Returned in the same shape as the input data.
+%
+% Author: Tanner Chas Dixon, tanner.dixon@ucsf.edu. Credit to Juan Anso for
+%             earlier version of the code.
+% Date last updated: February 10, 2022
 %---------------------------------------------------------
 
 function data_rcs = transformMVtoRCS(data_mv, amp_gain)
--- code/td_to_pb_testing/transformRCStoMV.m
+++ code/td_to_pb_testing/transformRCStoMV.m
@@ -1,21 +1,26 @@
 %% Function Name: data_mv = transformRCStoMV(data_rcs, amp_gain)
 %
-% Description: Converts data in units of mV to the internal unit 
-% representation used on the RC+S device.
+% Description: Converts data from the internal unit representation used on 
+% the RC+S device to units of mV.
 %
 % Inputs:
-%     data_mV : (num_samples, num_channels) array, or transpose
-%         Data, either time-domain or FFT amplitude, given in units of mV.
-%         If a two dimensional array is given, the result will be returned
-%         in the same shape.
+%     data_rcs : (num_samples, num_channels) array, or transpose
+%         Data, either time-domain or FFT amplitude, given in internal RC+S
+%         units. The result will be returned in the same shape.
 %     amp_gain : int, 
 %         Parameter indicating the channel gain represented by the
 %         cfg_config_data.dev.HT_sns_ampX_gain250_trim value in the
 %         DeviceSettings.json file, or the metaData.ampGains OpenMind
 %         output.
 %
-% Author: Tanner Chas Dixon, tanner.dixon@ucsf.edu
-% Date: February 3, 2022
+% Outputs:
+%     data_mV : (num_samples, num_channels) array, or transpose
+%         Data, either time-domain or FFT amplitude, given in units of mV.
+%         Returned in the same shape as the input data.
+%
+% Author: Tanner Chas Dixon, tanner.dixon@ucsf.edu. Credit to Juan Anso for
+%             earlier version of the code.
+% Date last updated: February 10, 2022
 %---------------------------------------------------------
 
 function data_mv = transformRCStoMV(data_rcs, amp_gain)
