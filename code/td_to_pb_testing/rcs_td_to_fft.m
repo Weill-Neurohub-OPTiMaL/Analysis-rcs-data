c0b1333773b62fab01f494cfb7ec9b81b94f1423
tcdixon4 <tcdixon44@gmail.com>
2022-02-10 17:20:51 -0800

    Modify to allow optional output in units of mV

--- code/td_to_pb_testing/rcs_td_to_fft.m
+++ code/td_to_pb_testing/rcs_td_to_fft.m
@@ -1,78 +1,94 @@
-%% Function Name: data_fft = rcs_td_to_fft(data_td, L, interval, fs_td, amp_gain)
+%% Function Name: rcs_td_to_fft()
 %
-% Description: Converts data in units of mV to the internal unit 
-% representation used on the RC+S device.
+% Description: Computes short-time FFT of Time-Domain data as it is
+% performed onboard the RC+S device. The result may optionally be converted
+% to match the logged FFT outputs in units of mV.
 %
 % Inputs:
-%     data_td : (num_samples, num_channels) array, or transpose
-%         Data, either time-domain or FFT amplitude, given in units of mV.
-%         If a two dimensional array is given, the result will be returned
-%         in the same shape.
-%     L : int, 
-%         Parameter indicating the channel gain represented by the
-%         cfg_config_data.dev.HT_sns_ampX_gain250_trim value in the
-%         DeviceSettings.json file, or the metaData.ampGains OpenMind
-%         output.
-%     interval : int, 
-%         Parameter indicating the channel gain represented by the
-%         cfg_config_data.dev.HT_sns_ampX_gain250_trim value in the
-%         DeviceSettings.json file, or the metaData.ampGains OpenMind
-%         output.
-%     fs_td : int, 
-%         Parameter indicating the channel gain represented by the
-%         cfg_config_data.dev.HT_sns_ampX_gain250_trim value in the
-%         DeviceSettings.json file, or the metaData.ampGains OpenMind
-%         output.
+%     data_td : (num_samples, 1) array, or transpose
+%         Time-Domain data, given in internal RC+S units.
+%     time_td : (num_samples, 1) array, or transpose
+%         Unix timestamps (in whole ms format) for the corresponding
+%         Time-Domain data samples.
+%     fs_td : int
+%         The Time-Domain sampling rate, in Hz.
+%     L : int, {64, 256, 1024}
+%         FFT size, in number of samples.
+%     interval : int 
+%         The interval, in ms, that the FFT window is shifted for producing
+%         each subsequent output sample.
+%     output_in_mv : optional boolean, default=False
+%         Boolean flag indicating whether to match the FFT output units
+%         to what is logged by the device (scaled mV).
 %
-% Author: Tanner Chas Dixon, tanner.dixon@ucsf.edu
-% Date: February 3, 2022
+% Outputs:
+%     data_fft : (num_windows, L) array
+%         FFT amplitude data given in internal RC+S units, or converted to 
+%         match the scaled mV units that the device outputs in data logs if
+%         specified by the `output_in_mv` parameter.
+%     time_fft : (num_windows, 1) array, or transpose
+%         Unix timestamps for the corresponding FFT data samples.
+%
+% Author: Tanner Chas Dixon, tanner.dixon@ucsf.edu. Credit to Juan Anso for
+%             earlier version of the code.
+% Date last updated: February 10, 2022
 %---------------------------------------------------------
 
-function [data_fft, timestamps] = rcs_td_to_fft(data_td, timestamps, L, ...
-                                       interval, fs_td, amp_gain, hann_win)
+function [data_fft, time_fft] = rcs_td_to_fft(data_td, time_td, fs_td,...
+                                       L, interval, hann_win, output_in_mv)
 
-% The actual FFT uses a smaller number of actual time-domain samples and
+% Validate function arguments and set defaults
+arguments
+    data_td {mustBeNumeric}
+    time_td {mustBeNumeric}
+    fs_td {mustBeInteger}
+    L {mustBeMember(L,[64,256,1024])} 
+    interval {mustBeInteger}
+    hann_win {mustBeNumeric}
+    output_in_mv {mustBeNumericOrLogical} = false
+end
+
+% The actual FFT uses a smaller number of true time-domain samples and
 % zero-pads the remainder
 switch L
     case 64
-        L_non_zero = 62;
+        L_non_zero = 62; % on the device this alternates between 62 and 63
     case 256
         L_non_zero = 250;
     case 1024
         L_non_zero = 1000;
 end
 
-% Linearly interpolate over nan-values
+% Linearly interpolate over NaN-values
 nan_mask = isnan(data_td);
 idx = 1:numel(data_td);
 data_td(nan_mask) = interp1(idx(~nan_mask), data_td(~nan_mask), ...
     idx(nan_mask));
-% data_td = transformMVtoRCS(data_td, amp_gain);
 
-% Select all FFT window edges
+% Pre-select all FFT window edges
 mean_window_shift = interval*fs_td/1000;
 num_windows = floor((length(data_td)-L)/mean_window_shift)...
     + 1;
 window_stops = ceil((0:num_windows-1)*mean_window_shift) ...
     + L_non_zero;
 window_starts = window_stops - L_non_zero + 1;
-timestamps = timestamps(window_stops);
-% data_fft = zeros(num_windows, L/2);
+time_fft = time_td(window_stops);
 data_fft = zeros(num_windows, L);
+% Iterate over FFT windows
 for s = 1:length(window_stops)
-    % select the time-domain window and zero-pad remaining points
+    % Select the time-domain window and zero-pad remaining points
     td_window = zeros(1,L);
     td_window(1:L_non_zero) = ...
         data_td(window_starts(s):window_stops(s))';
-    % take the FFT to get two-sided amplitude spectrum
+    % Take the FFT and calculate complex magnitudes
     current_fft = fft(td_window.*hann_win, L);
-    %     current_fft = abs(current_fft) / sqrt(2*L);
-%     current_fft = 2*abs(current_fft)/L;
-%     current_fft = 2*abs(current_fft)/sum(hann_win);
     current_fft = abs(current_fft);
-%     current_fft = current_fft(1:L/2);
-    %     current_fft = transformRCStoMV(current_fft, amp_gain); % this should be removed after testing. we can do it on the back end, but leaving it out will allow this function to be reused in a wrapper that does the full TD->FFT->PB
     data_fft(s,:) = current_fft;
-
 end
+
+% Convert the units to match the logged output, if desired
+if output_in_mv
+    data_fft = 4*data_fft/L;
+end
+
 end
